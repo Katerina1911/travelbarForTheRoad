bc49ad4d6e04f66ce15b5575790e97d34b4bd3df
Katerina Bondarenko <bondarenko.kate91@gmail.com>
2024-12-14 20:29:55 +0300

    start

---
+++ src/main/java/ru/bondarenko/travelbar/travelbarfortheroad/TravelbarForTheRoadApplication.java
@@ -0,0 +1,13 @@
+package ru.bondarenko.travelbar.travelbarfortheroad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelbarForTheRoadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelbarForTheRoadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/bondarenko/travelbar/travelbarfortheroad/TravelbarForTheRoadApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bondarenko.travelbar.travelbarfortheroad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelbarForTheRoadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
